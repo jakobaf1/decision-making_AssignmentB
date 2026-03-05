b876ceb8be1d70d1f169f8036f03fca52ef2a833
jakobaf1 <jakobaf1@hotmail.com>
2026-03-05 23:27:09 +0100

    gathered all initial files given

---
+++ task6_environment.py
@@ -0,0 +1,20 @@
+# TODO: Load Data
+
+# TODO: Define environment class
+
+# TODO: Define dummy policy function
+# def dummy_policy():
+#     return _
+
+# TODO: define the simulation function
+# def evaluate_policy(policy):
+#     for e in experiments:
+#         reset environment
+#         simulate T timesteps
+#         record cost
+#     return average cost
+
+#TODO: Add the main function that runs program
+# if __name__ == "__main__":
+#     avg_cost = evaluate_policy(dummy_policy)
+#     print(avg_cost)
